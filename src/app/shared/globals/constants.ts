96af447580ee4a9e73e26b4cd0e8f3d9f474cb7a
Jose Victor Hendz Martins <jose.victor@infoworld.com.br>
2018-10-18 11:47:22 -0300

    Add baseUrl online

--- src/app/shared/globals/constants.ts
+++ src/app/shared/globals/constants.ts
@@ -1,6 +1,6 @@
 import { environment } from '../../../environments/environment';
 
 // const BASE_URL = 'https://safraonline-api.herokuapp.com';
-const BASE_URL =  'http://localhost:3000'; // (!environment.production) ? 'http://localhost:3000' : 'https://safraonline-api.herokuapp.com';
+const BASE_URL = (!environment.production) ? 'http://localhost:3000' : 'https://fabric-editor-back.herokuapp.com';
 
 export { BASE_URL };
